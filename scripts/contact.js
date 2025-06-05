78706a23723aa6174a50f9d784d21dc3098422a6
valbiten <adrian.n.s.1@gmail.com>
2025-06-05 22:44:08 +0200

    Delete scripts directory

--- scripts/contact.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const formData = new FormData(this);
-  alert('Formulario enviado. Nos pondremos en contacto contigo.');
-  this.reset();
-});
